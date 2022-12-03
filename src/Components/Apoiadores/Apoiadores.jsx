20172755a473cc4682e9b9dc652d6cf899e8ba7a
Casimiro Thompson <casimirothompson@gmail.com>
2022-12-03 10:26:06 -0300

    .

--- src/Components/Apoiadores/Apoiadores.jsx
+++ src/Components/Apoiadores/Apoiadores.jsx
@@ -3,19 +3,20 @@ import ApoiadoresPt2 from './ApoiadoresPt2'
 import ApoiadoresPt3 from './ApoiadoresPt3'
 import ApoiadoresPt4 from './ApoiadoresPt4'
 
-function Apoiadores(){
-    return(
+function Apoiadores() {
+    return (
         <>
-       <section className={styles.ApoiadoresPt1}>
+            <div className={styles.headerInvisivel}></div>
+            <section className={styles.ApoiadoresPt1}>
 
-       <div className={styles.txt}>
-            <h1>
-            Vantagens de ser um apoiador do Projeto Donare
-            </h1>
-        </div>
-          
-       </section>
-       <ApoiadoresPt2 />
+                <div className={styles.txt}>
+                    <h1>
+                        Vantagens de ser um apoiador do Projeto Donare
+                    </h1>
+                </div>
+
+            </section>
+            <ApoiadoresPt2 />
             <ApoiadoresPt3 />
             <ApoiadoresPt4 />
         </>
--- src/Components/Voluntarios/Voluntario.jsx
+++ src/Components/Voluntarios/Voluntario.jsx
@@ -1,23 +1,20 @@
+import fotografias from '../../Images/fotografias.png'
 import styles from '../../Css/Voluntario_css/Voluntario.module.css'
-import imagemvoluntario from '../../Images/img_voluntario1.png'
 import VoluntarioPt2 from './VoluntarioPt2'
-import VoluntarioPt3 from './VoluntarioPt3'
 
 function Voluntario() {
     return (
         <>
-            <section className={styles.page1}>
-                <div className={styles.txt}>
-                    <h1>Existem muitas formas de ajudar o próximo</h1>
-                    <h2>Veja como você pode se voluntariar</h2>
-                </div>
-                <div className={styles.container_imagem}>
-                    <img src={imagemvoluntario} alt="" />
+            <div className={styles.headerInvisivel}></div>
+            <section className={styles.VoluntarioPT1}>
+                <p>
+                    Existem muitas formas <br></br> de ajudar o proximo
+                </p>
+                <div className={styles.containerFotografias}>
+                    <img src={fotografias} alt="" />
                 </div>
             </section>
             <VoluntarioPt2 />
-            <VoluntarioPt3 />
-
         </>
     )
 }
