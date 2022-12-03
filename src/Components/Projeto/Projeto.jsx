78c817f0bf7f059d37962a9a33c0a3edba64e261
Casimiro Thompson <casimirothompson@gmail.com>
2022-12-03 06:00:29 -0300

    projeto finalizado

--- src/Components/Projeto/Projeto.jsx
+++ src/Components/Projeto/Projeto.jsx
@@ -5,14 +5,16 @@ import ProjetoPt3 from './ProjetoPt3';
 function Projeto() {
     return (
         <>
+            <section className={styles.headerInvisivel}></section>
+            <section className={styles.projetoPT1}>
+                <section className={styles.projetoPT1Esquerda}>
+                    <h1 className={styles.tituloH1}><span className={styles.corRoxa}>O projeto </span><br /><span className={styles.corVermelha}>Donare</span></h1>
+                    <p className={styles.textoTamanho1}>O Projeto nasceu da pergunta: Por que mais da metade do Brasil não possui o que comer nas três refeições?
+                    </p>
+                </section>
 
-            <section className={styles.Projeto}>
-                <div className={styles.txt}>
-                    <h1> <span className={styles.cor_roxa}>O projeto</span> <span className={styles.cor_vermelha}>Donare</span> </h1>
-                    <br />
-                    <p>O Projeto nasceu da pergunta: Por que mais da metade do Brasil não possui o que comer nas três refeições?</p>
-                </div>
-            </section>
+                <section className={styles.projetoPT1Direito}></section>
+            </section >
             <ProjetoPt2 />
             <ProjetoPt3 />
         </>
--- src/Components/Projeto/ProjetoPt2.jsx
+++ src/Components/Projeto/ProjetoPt2.jsx
@@ -1,18 +1,24 @@
 import styles from '../../Css/Projeto_css/ProjetoPt2.module.css'
 function ProjetoPt2() {
     return (
-        <section className={styles.ProjetoPt2}>
-            <section className={styles.projetoPt2Esquerda}>
-                <div className={styles.blocoEsquerdo}>
-                    <h1>O desperdicio, a fome e a pandemia</h1>
-                    <hr />
-                    <p>Os mercados e restaurantes são responsaveis por x quantidades de alimentos em boas condiçoes que são descartados, boa parte desses alimentos são jogados no lixo por x motivos. Nós buscamos diminuir a </p>
+        <section className={styles.projetoPT2}>
+            <section className={styles.projetoPT2Esquerda}>
+                <div className={styles.blocoTexto}>
+                    <h1 className={styles.tituloh1}>O desperdicio, a fome e a pandemia</h1>
+                    <div className={styles.linha}></div>
+                    <p className={styles.textoTamanho1}>Os mercados e restaurantes são responsaveis por x quantidades de alimentos em boas condiçoes que são descartados, boa parte desses alimentos são jogados no lixo por x motivos. Nós buscamos diminuir a </p>
                 </div>
             </section>
-            <section className={styles.projetoPt2Direita}>
-                <p className={styles.txtEsquerda}>Evitar o desperdicio</p>
-                <p className={styles.txtDireito}>Ajudar quem precisa no momento que mais precisa</p>
-                <p className={styles.txtEsquerda}>Levar o alimemento até quem precisa comer</p>
+            <section className={styles.projetoPT2Direito}>
+                <p className={styles.textoEsquerdo1}>
+                Evitar o desperdicio
+                </p>
+                <p className={styles.textoDireito2}>
+                Ajudar quem precisa no <br></br> momento que mais precisa
+                </p>
+                <p className={styles.textoEsquerdo2}>
+                Levar o alimemento até <br></br>quem precisa comer
+                </p>
             </section>
         </section>
     )
