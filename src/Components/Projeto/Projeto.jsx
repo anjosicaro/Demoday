625311f40fe3c81e855fd7499f8437f4db6f4ff4
Casimiro Thompson <casimirothompson@gmail.com>
2022-12-02 22:25:11 -0300

    fazendo parte 2

--- src/Components/Projeto/Projeto.jsx
+++ src/Components/Projeto/Projeto.jsx
@@ -2,19 +2,19 @@ import styles from '../../Css/Projeto_css/Projeto.module.css'
 import ProjetoPt2 from './ProjetoPt2'
 import ProjetoPt3 from './ProjetoPt3';
 
-function Projeto(){
-    return(
+function Projeto() {
+    return (
         <>
-        
-        <section className={styles.Projeto}>
-         <div className={styles.txt}>
-         <h1>O Projeto Donare</h1>
-            <br />
-           <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa quis debitis facilis explicabo vero aspernatur vel voluptatibus quo, magni nulla voluptatum rem necessitatibus, doloribus obcaecati sapiente illum corporis id consequuntur.</p>
-         </div>
-        </section>
-        <ProjetoPt2 />
-        <ProjetoPt3 />
+
+            <section className={styles.Projeto}>
+                <div className={styles.txt}>
+                    <h1> <span className={styles.cor_roxa}>O projeto</span> <span className={styles.cor_vermelha}>Donare</span> </h1>
+                    <br />
+                    <p>O Projeto nasceu da pergunta: Por que mais da metade do Brasil não possui o que comer nas três refeições?</p>
+                </div>
+            </section>
+            <ProjetoPt2 />
+            <ProjetoPt3 />
         </>
     )
 }
--- src/Components/Projeto/ProjetoPt2.jsx
+++ src/Components/Projeto/ProjetoPt2.jsx
@@ -1,8 +1,19 @@
 import styles from '../../Css/Projeto_css/ProjetoPt2.module.css'
-function ProjetoPt2(){
-    return(
+function ProjetoPt2() {
+    return (
         <section className={styles.ProjetoPt2}>
-
+            <section className={styles.projetoPt2Esquerda}>
+                <div className={styles.blocoEsquerdo}>
+                    <h1>O desperdicio, a fome e a pandemia</h1>
+                    <hr />
+                    <p>Os mercados e restaurantes são responsaveis por x quantidades de alimentos em boas condiçoes que são descartados, boa parte desses alimentos são jogados no lixo por x motivos. Nós buscamos diminuir a </p>
+                </div>
+            </section>
+            <section className={styles.projetoPt2Direita}>
+                <p className={styles.txtEsquerda}>Evitar o desperdicio</p>
+                <p className={styles.txtDireito}>Ajudar quem precisa no momento que mais precisa</p>
+                <p className={styles.txtEsquerda}>Levar o alimemento até quem precisa comer</p>
+            </section>
         </section>
     )
 }
