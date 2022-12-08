7f5bac487fa1b4ac537d322a212b59ec68557c8c
Casimiro Thompson <casimirothompson@gmail.com>
2022-12-08 00:52:08 -0300

    refazendo parte 3 da pagina de Apoiadores

--- src/Components/Apoiadores/ApoiadoresPt3.jsx
+++ src/Components/Apoiadores/ApoiadoresPt3.jsx
@@ -1,30 +1,17 @@
 import styles from '../../Css/Apoiadores_css/ApoiadoresPt3.module.css'
+import fotoProa from '../../Images/fotoProa.png'
+import fotoSenac from '../../Images/fotoSenac.png'
+import fotoFAAP from '../../Images/fotoFAAP.png'
 
-function ApoiadoresPt3(){
-    return(
-        <section className={styles.ApoiadoresPt2}>
-            <h1>
-            Empresas Parceiras do projeto
-            </h1>
-            <div className={styles.containerMercados}>
-                <div className={styles.Mercados}>
-                <div className={styles.divAmarela}></div>
-                <div className={styles.divVermelha}></div>
-                <div className={styles.divAmarela}></div>
-                <div className={styles.divVermelha}></div>
-                </div>
-               <div className={styles.Mercados}>
-               <div className={styles.divVermelha}></div>
-                <div className={styles.divAmarela}></div>
-                <div className={styles.divVermelha}></div>
-                <div className={styles.divAmarela}></div>
-               </div>
-              <div className={styles.Mercados}>
-              <div className={styles.divAmarela}></div>
-                <div className={styles.divVermelha}></div>
-                <div className={styles.divAmarela}></div>
-                <div className={styles.divVermelha}></div>
-              </div>
+function ApoiadoresPt3() {
+    return (
+        <section className={styles.ApoiadoresPT3}>
+            <h1>Empresas Parceiras do projeto</h1>
+
+            <div className={styles.containerApoiadores}>
+                <div><img src={fotoProa} alt="" /></div>
+                <div><img src={fotoSenac} alt="" /></div>
+                <div><img src={fotoFAAP} alt="" /></div>
             </div>
         </section>
     );
--- src/Components/Apoiadores/ApoiadoresPt4.jsx
+++ src/Components/Apoiadores/ApoiadoresPt4.jsx
@@ -1,12 +1,12 @@
 import styles from '../../Css/Apoiadores_css/ApoiadoresPt4.module.css'
 import { Link } from 'react-router-dom'
 
-function ApoiadoresPt4(){
-    return(
+function ApoiadoresPt4() {
+    return (
         <>
             <section className={styles.ApoiadoresPt3}>
                 <p>
-                Seja também um apoiador dessa causa
+                    Seja também um apoiador dessa causa
                 </p>
                 <Link>APOIAR</Link>
             </section>
